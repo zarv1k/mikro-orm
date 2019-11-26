c82113c95f8c85ee1a988328d4e8155b7ed93fb8
Martin Adámek <banan23@gmail.com>
2019-11-26 16:54:02 +0000

    Deploy website version based on 383f0dee2c739d2b56d8c929c0ea5313180b39aa

---
+++ 79c1ba4c.c4cafdb4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[75],{210:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"oop","name":"oop","count":1,"permalink":"/blog/tags/oop"}')}}]);
