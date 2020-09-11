252b555f05420977e3f5c71707039822a3b2d055
Martin Adámek <banan23@gmail.com>
2020-09-11 17:23:21 +0000

    Deploy website - based on 7576c7fb4e18686e8384c8632491841e2a19a2ad

---
+++ bcbbf42e.97bc440d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[255],{312:function(p){p.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"typescript","name":"typescript","count":4,"permalink":"/blog/tags/typescript"}')}}]);
